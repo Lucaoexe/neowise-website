8adb8fb569c0dd5f5884c3c5ac1dc80133225405
Lucas <lucasdsantospds@gmail.com>
2020-08-20 13:02:41 -0300

    Menu asp
    Adicionado Menu asp

--- wbaNeowise/PGMestre.Master.designer.cs
+++ wbaNeowise/PGMestre.Master.designer.cs
@@ -33,6 +33,15 @@ namespace wbaNeowise
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
         /// <summary>
+        /// Controle Menu1.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Menu Menu1;
+
+        /// <summary>
         /// Controle ContentPlaceHolder1.
         /// </summary>
         /// <remarks>
